e312f237a1a2fdbae9cf84cfe2c14bd0cd3b6665
Robinson Rojas <rrojas@itteamcorp.com>
2022-04-30 18:12:41 -0500

    refactor(api): Añadir relaciones faltantes

--- api/models/File.js
+++ api/models/File.js
@@ -2,7 +2,11 @@ const { Schema, model } = require('mongoose')
 
 const fileSchema = new Schema({
   data: Buffer,
-  content_type: String
+  content_type: String,
+  ticket: {
+    type: Schema.Types.ObjectId,
+    ref: 'Ticket'
+  }
 })
 
 const File = model('File', fileSchema)
